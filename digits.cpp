4a35bd903ce84590852b3d0d559fa76ce82a2704
vanshika704 <sharmavanshi704@gmail.com>
2024-07-01 13:12:05 +0530

    added strivers code as well

--- digits.cpp
+++ digits.cpp
@@ -1,48 +1,49 @@
 #include <iostream>
 using namespace std;
+// using reversed number
+// int main()
+// {
+//     int number;
+//     cout << "Enter number: ";
+//     cin >> number;
 
-int main()
-{
-    int number;
-    cout << "Enter number: ";
-    cin >> number;
+//     if (number < 0)
+//     {
+//         cout << "Please enter a non-negative number." << endl;
+//         return 1;
+//     }
 
-    if (number < 0)
-    {
-        cout << "Please enter a non-negative number." << endl;
-        return 1;
-    }
+//     if (number == 0)
+//     {
+//         cout << "Digits: 0" << endl;
+//         return 0;
+//     }
 
-    if (number == 0)
-    {
-        cout << "Digits: 0" << endl;
-        return 0;
-    }
+//     // Reverse the number
+//     int reversedNumber = 0;
+//     int originalNumber = number;
 
-    // Reverse the number
-    int reversedNumber = 0;
-    int originalNumber = number;
+//     while (number > 0)
+//     {
+//         int lastDigit = number % 10;
+//         reversedNumber = reversedNumber * 10 + lastDigit;
+//         number = number / 10;
+//     }
 
-    while (number > 0)
-    {
-        int lastDigit = number % 10;
-        reversedNumber = reversedNumber * 10 + lastDigit;
-        number = number / 10;
-    }
+//     // Print the digits of the reversed number
+//     cout << "Digits: ";
+//     while (reversedNumber > 0)
+//     {
+//         int digit = reversedNumber % 10;
+//         cout << digit << " ";
+//         reversedNumber = reversedNumber / 10;
+//     }
 
-    // Print the digits of the reversed number
-    cout << "Digits: ";
-    while (reversedNumber > 0)
-    {
-        int digit = reversedNumber % 10;
-        cout << digit << " ";
-        reversedNumber = reversedNumber / 10;
-    }
+//     cout << endl;
+//     return 0;
+// }
 
-    cout << endl;
-    return 0;
-}
-
+// using vectors
 // #include <iostream>
 // #include <vector>
 // using namespace std;
@@ -83,3 +84,17 @@ int main()
 //     cout << endl;
 //     return 0;
 // }
+
+// strivers code ;
+int main()
+{
+    int number;
+    cout << "enter number";
+    cin >> number;
+    while (number > 0)
+    {
+        int lastdigit = number % 10;
+        cout << lastdigit << endl;
+        number = number / 10;
+    }
+}
