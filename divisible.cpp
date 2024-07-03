dd0246b1238fd6a1e9bc09294f1a63e112cd7f4e
vanshika704 <sharmavanshi704@gmail.com>
2024-07-03 23:34:04 +0530

    .

--- divisible.cpp
+++ divisible.cpp
@@ -4,11 +4,11 @@ int main()
 {
     int number;
     cin >> number;
-    // for (int n = 1; n <= number; n++)
-    // {
-    //     if (number % n == 0)
-    //     {
-    //         cout << n << endl;
-    //     }
-    // }
+    for (int n = 1; n <= number; n++)
+    {
+        if (number % n == 0)
+        {
+            cout << n << endl;
+        }
+    }
 }
