6902735df6d82fe1fa5c0022b1d9d3c819dc7a9c
vanshika704 <sharmavanshi704@gmail.com>
2024-07-19 23:09:04 +0530

    .

---
+++ bubblesort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+void selectionsort(int array[], int n) // function bnaya humne sselection sort naam ka , ume do values pass ki h , array , int
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (array[j] < array[minIndex])
+            {
+                minIndex = j;
+            }
+        }
+        int temp = array[minIndex];
+        array[minIndex] = array[i];
+        array[i] = temp;
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter elements of array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i]; // Fix: use arr[i] instead of arr[n]
+    }
+    selectionsort(arr, n);
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
